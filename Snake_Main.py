d468fb0fbca9ad38fbf8ec8db9771a954cd3e8c8
Zeke19066 <77519955+Zeke19066@users.noreply.github.com>
2021-11-03 00:15:32 -0700

    Full Implementation, forbbiden list

--- Snake_Main.py
+++ Snake_Main.py
@@ -3,29 +3,37 @@ Controls:
     - Arrows to move
     - +/- to control game speed
 Configured for 100x100 game window.
+
+
+Note: Move food bool section from loop into generator function for cleanliness and to call
+after each snake moves.
+
+
+EVERYTHING IN Y,X FORMAT
+
 '''
 import pygame
 import numpy as np
 import time
-import random
 import os
 import Snake_Game_AI as AI
+from collections import deque
+
+from Custom_A_Star import pybind11module as Custom_A_Star
 
 class SnakeGame():
     def __init__(self):
         print('Game Initialized!')
         self.ai_control = True #no human control.
-        self.ai = AI.SnekAI()
-        self.resolution_width = 300
-        self.resolution_height = 200
-        self.pixel_size = 1
-        self.snake_speed = 500
+        self.ai_players = 1 #how many snek?
+
+        self.resolution_width = 100
+        self.resolution_height = 50
+        self.pixel_size = 10
+        self.snake_speed = 100
         self.final_speed = 0
         self.game_close = False
 
-        self.snake_head = []
-        self.snake_body = []
-        self.forbidden_coords = [] #this will contain snake bodies(walls)
         self.food = []
         self.food_total_count = 0 #how many foods have we seen total?
         self.food_game_count = 0 #how many foods have we seen before we lost this game?
@@ -53,14 +61,21 @@ class SnakeGame():
             "blue": (50, 153, 213),
             }
 
-        
+        self.player_1 = Snek_Actor(self.color_dict["green"], 1, self.resolution_width, self.resolution_height)
+        if self.ai_players == 2:
+            self.player_2 = Snek_Actor(self.color_dict["blue"], 2, self.resolution_width, self.resolution_height)
+        if self.ai_players == 3:
+            self.player_2 = Snek_Actor(self.color_dict["blue"], 2, self.resolution_width, self.resolution_height)
+            self.player_3 = Snek_Actor(self.color_dict["yellow"], 3, self.resolution_width, self.resolution_height)
+
+   
     def score_generator(self, score):
         value = self.score_font.render("Your Score: " + str(score), True, self.color_dict["white"])
         self.screen.blit(value, [0, 0]) # Draw the score onto the screen at these coordinates.
 
-    def snake_plotter(self, snake_body, player):
-        for x in snake_body:
-            pygame.draw.rect(self.screen, self.color_dict["green"], [x[0], x[1], 1, 1])
+    def snake_plotter(self, snake_player):
+        for pixel in snake_player.full_snek:
+            pygame.draw.rect(self.screen, snake_player.color, [pixel[1], pixel[0], 1, 1])
 
     def message(self, msg, color):
         mesg = self.font_style.render(msg, True, color)
@@ -68,35 +83,46 @@ class SnakeGame():
         self.screen.blit(mesg, [0, int(self.resolution_height / 3)])
 
     def food_generator(self):
-        
-        food_xy = self.snake_body[0]
-        while food_xy in self.snake_body:
-            food_xy=[]
+        forbidden_spawn = self.forbidden_builder("Spawn")
+
+        self.food = forbidden_spawn[0]
+        while self.food in forbidden_spawn:
+            self.food=[]
             foodx = np.random.randint(0, self.resolution_width)
             foody = np.random.randint(0, self.resolution_height)
-            food_xy.append(foodx)
-            food_xy.append(foody)
+            self.food = [foody, foodx]
 
+        self.player_1.food = self.food
+        if self.ai_players == 2:
+            self.player_2.food = self.food
+        if self.ai_players == 3:
+            self.player_2.food = self.food
+            self.player_3.food = self.food
 
-        self.food = food_xy #let's update the global variable for export to the AI.
         self.food_game_count += 1
         self.food_total_count += 1
-        return foodx, foody
+
+        pygame.draw.rect(self.screen, self.color_dict["red"], [foodx, foody, 1, 1])
+        self.dis.blit(pygame.transform.scale(self.screen, self.dis.get_rect().size), (0, 0))
+        pygame.display.update()
+
+        #Call Snake Plotter for every snake.
+        self.snake_plotter(self.player_1)
+        if self.ai_players == 2:
+            self.snake_plotter(self.player_2)
+        if self.ai_players == 3:
+            self.snake_plotter(self.player_2)
+            self.snake_plotter(self.player_3)
+
+
 
     def gameLoop(self):
         speed_mod = 0 # Speed adjustments made after the game has started.
         terminal_bool = False
-        x1 = np.random.randint(1,self.resolution_width)
-        y1 = np.random.randint(1,self.resolution_height)
-        x1_change = 0
-        y1_change = 0
-        self.snake_body = []
-        self.snake_body.append([x1,y1])
-        snake_length = 1
-        foodx, foody = self.food_generator() #this is when we request the starting food.
+        self.food_generator() #this is when we request the starting food.
 
         while not terminal_bool:
-            x1_change, y1_change = 0,0
+            food_bool = False
 
             # Exit Sequence
             if self.game_close == True:
@@ -151,59 +177,43 @@ class SnakeGame():
                 
             #process ai control
             if self.ai_control and (self.cycle!=1):
-                screen_array = pygame.surfarray.array3d(self.screen) 
-                #print(screen_array.shape)
 
-                action = self.ai.switchboard(screen_array)
-                #print(action)
-                if action == "Left":
-                    x1_change = -1
-                    y1_change = 0
-                elif action == "Right":
-                    x1_change = 1
-                    y1_change = 0
-                elif action == "Up":
-                    y1_change = -1
-                    x1_change = 0
-                elif action == "Down":
-                    y1_change = 1
-                    x1_change = 0
+                forbidden_path = self.forbidden_builder("Path", player=1)
+                food_bool = self.player_1.snek_step(forbidden_path)
+                if food_bool:#trigger the new food condition
+                    self.food_generator()  # We generate the next food before updating the screen.
                 
+                if self.ai_players == 2:
+                    forbidden_path = self.forbidden_builder("Path", player=2)
+                    food_bool = self.player_2.snek_step(forbidden_path)
+                    if food_bool:#trigger the new food condition
+                        self.food_generator()  # We generate the next food before updating the screen.
+                if self.ai_players == 3:
+                    forbidden_path = self.forbidden_builder("Path", player=2)
+                    food_bool = self.player_2.snek_step(forbidden_path)
+                    if food_bool:#trigger the new food condition
+                        self.food_generator()  # We generate the next food before updating the screen.
+                    forbidden_path = self.forbidden_builder("Path", player=3)
+                    food_bool = self.player_3.snek_step(forbidden_path)
+                    if food_bool:#trigger the new food condition
+                        self.food_generator()  # We generate the next food before updating the screen.
 
-            #out of bounds
-            if (x1 >= self.resolution_width or x1 < 0) or (y1 >= self.resolution_height or y1 < 0):
-                self.game_close = True
-                self.gameLoop()
-            
-            #apply the modification
-            x1 += x1_change
-            y1 += y1_change
-
+                
             self.screen.fill(self.color_dict["black"])
-            pygame.draw.rect(self.screen, self.color_dict["red"], [foodx, foody, 1, 1])
+            pygame.draw.rect(self.screen, self.color_dict["red"], [self.food[1], self.food[0], 1, 1])
             
-            #Account for the head, which is the decision point
-            self.snake_head = []
-            self.snake_head.append(x1)
-            self.snake_head.append(y1)
-            self.snake_body.append(self.snake_head)
-            
-            if len(self.snake_body) > snake_length:
-                del self.snake_body[0]
-            # SELF-CRASH: Last entry (:-1) is the head position. If a segment's coordinates match the head's coordinates, Game Over.
-            for x in self.snake_body[:-1]:
-                if x == self.snake_head:
-                    self.game_close = True
-            self.snake_plotter(self.snake_body, player=1)
+            #Call Snake Plotter for every snake.
+            self.snake_plotter(self.player_1)
+            if self.ai_players == 2:
+                self.snake_plotter(self.player_2)
+            if self.ai_players == 3:
+                self.snake_plotter(self.player_2)
+                self.snake_plotter(self.player_3)
+
             #self.score_generator(snake_length - 1)
             self.dis.blit(pygame.transform.scale(self.screen, self.dis.get_rect().size), (0, 0))
             pygame.display.update()
-            if x1 == foodx and y1 == foody:
-                foodx, foody = self.food_generator()  # We generate the next food before updating the screen.
-                pygame.draw.rect(self.screen, self.color_dict["red"], [foodx, foody, 1, 1])
-                self.dis.blit(pygame.transform.scale(self.screen, self.dis.get_rect().size), (0, 0))
-                pygame.display.update()
-                snake_length += 1
+            
             self.final_speed = self.snake_speed+speed_mod
             self.clock.tick(self.final_speed)
             self.cycle += 1
@@ -211,6 +221,125 @@ class SnakeGame():
     
         pygame.quit()
         quit()
+
+    def forbidden_builder(self, mode, player=0):
+        #spawn includes all occupied squares; path excludes head & food
+
+        forbidden_list = []
+
+        if mode == "Spawn":
+            forbidden_list += self.player_1.full_snek
+            if self.ai_players == 2:
+                forbidden_list += self.player_2.full_snek
+            if self.ai_players == 3:
+                forbidden_list += self.player_2.full_snek
+                forbidden_list += self.player_3.full_snek
+            forbidden_list += self.food
+
+        elif mode == "Path":
+            if player == 1:
+                forbidden_list += list(self.player_1.body) #just the body
+            elif player != 1:
+                forbidden_list += self.player_1.full_snek #full snek
+            
+            if self.ai_players == 2:
+                if player == 2:
+                    forbidden_list += list(self.player_2.body) #just the body
+                elif player != 2:
+                    forbidden_list += self.player_2.full_snek #full snek
+            
+            if self.ai_players == 3:
+                if player == 2:
+                    forbidden_list += list(self.player_2.body) #just the body
+                elif player != 2:
+                    forbidden_list += self.player_2.full_snek #full snek
+                if player == 3:
+                    forbidden_list += list(self.player_3.body) #just the body
+                elif player != 3:
+                    forbidden_list += self.player_3.full_snek #full snek
+                        
+        return forbidden_list
+
+class Snek_Actor():
+    def __init__(self, color, player_num, resolution_width, resolution_height):
+        self.color = color
+        self.player_num = player_num
+        self.resolution_height = resolution_height
+        self.resolution_width = resolution_width
+        self.dead_bool = False
+        self.ai = AI.SnekAI()
+
+        self.x1 = np.random.randint(1,self.resolution_width)
+        self.y1 = np.random.randint(1,self.resolution_height)
+        self.x1_change = 0
+        self.y1_change = 0
+
+        self.food = [] #this will be overwritten each time food is spawned.
+        self.snek_length = 0 #Note!!!! doesnt count the head!
+        self.head = [self.y1, self.x1]
+        self.body = deque(maxlen=self.snek_length) #initialized empty.
+        self.full_snek = list(self.body.copy()) #a combination of body and head.
+        self.full_snek.append(self.head)
+
+    def snek_step(self, forbidden_path):
+        """
+        Collision is determined here
+        forbidden_spawn contains all occupied coords except head & food (start & finish)
+        if we got the food, that's determined here.
+        """
+        food_bool = False
+        #if we're dead, do nothing.
+        if self.dead_bool:
+            return food_bool
+
+        if forbidden_path == []:
+            forbidden_path = [[-1,-1]]
+
+
+        #reverse the head & food.[::-1]
+        action = Custom_A_Star.launcher(self.resolution_height, self.resolution_width,
+                                    forbidden_path, self.head, self.food)
+
+        #print(f"{action}  Head:{self.head}   Food:{self.food}  {forbidden_path}")
+
+        if action == "Left":
+            x1_change = -1
+            y1_change = 0
+        elif action == "Right":
+            x1_change = 1
+            y1_change = 0
+        elif action == "Up":
+            y1_change = -1
+            x1_change = 0
+        elif action == "Down":
+            y1_change = 1
+            x1_change = 0
+
+        #add old head to the body que; nulls if length equals zero.
+        if self.snek_length > 0:
+            self.body.append(self.head.copy())
+
+        #apply the modification to head position.
+        self.head[1] += x1_change
+        if self.head[1] not in range(0, self.resolution_width+1):
+            self.dead_bool = True
+            self.color = (255, 255, 255) #ghost
+        self.head[0] += y1_change
+        if self.head[0] not in range(0, self.resolution_height+1):
+            self.dead_bool = True
+            self.color = (255, 255, 255) #ghost
+
+        #we got the food; snek gets longer
+        if self.head == self.food:
+            food_bool = True #will trigger new food.
+            self.snek_length += 1
+            temp = list(self.body.copy())
+            self.body = deque(temp, maxlen=self.snek_length)
+
+        self.full_snek = list(self.body.copy()) #a combination of body and head.
+        self.full_snek.append(self.head)
+
+        return food_bool
 
 
 
